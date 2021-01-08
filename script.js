f243d8eebff6c1fd02c367cca8d5de095bf2696c
joliehehehe <s10205048@connect.np.edu.sg>
2021-01-09 04:27:19 +0800

    Update #6: Allcards changed layout

--- script.js
+++ script.js
@@ -15,38 +15,37 @@ $(document).ready(function(){
         let display = "";
         for(var i = 0; i < data.results.length;i++){
           let results = data.results[i];
+          
+          display = `${display} 
+              <div class="col-sm">
+                <div class="card">
+                <div class="d-flex justify-content-center">
+                  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" id="cardimage1" />
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" id="cardimage2" />
+                      </div>
+                      <div id="stats">
+                       <h5>Statistics</h5>
+                       <p>Performance: ${results.performance_trained_max}<br>
+                       Technique: ${results.technique_trained_max}<br>
+                       Visual: ${results.visual_trained_max}</p>
+                      </div>
+                    </div>
+                  </div>
 
-          display = `${display} <div class="card" style="width: 25rem;">
-                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" id="cardimage1" />
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" id="cardimage2" />
-                  </div>
-                  <div id="stats">
-                    <p>Performance: ${results.performance_trained_max}</p>
-                    <p>Technique: ${results.technique_trained_max}</p>
-                    <p>Visual: ${results.visual_trained_max}</p>
+                  <div class="card-body">
+                    <h5 class="card-title">${results.name} (${results.japanese_name})</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">${results.skill_name} (${results.japanese_skill_name})</h6>
+                    <p class="card-text">${results.full_skill}</p>
+                    <button type="button" class="btn btn-dark">Click to add to My List</button>
                   </div>
                 </div>
-                
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                  <span class="sr-only">Next</span>
-                </a>
-            </div>
-            <div class="card-body">
-            <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
-            <h6 class="card-subtitle mb-2 text-muted">Skill: ${results.skill_name} (${results.japanese_skill_name})</h6>
-            <p class="card-text">${results.full_skill}</p>
-            </div>
-            </div>`
+                </div>
+              </div>`
         }//end loop
         //place our entire loop content into the container
         $('#card-container').html(display);
