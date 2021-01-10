8018a4efc0d45756bb8ef7dde6cf6092a376bd09
joliehehehe <s10205048@connect.np.edu.sg>
2021-01-10 17:18:20 +0800

    Update #10: Sort by rarity done

--- script.js
+++ script.js
@@ -248,6 +248,194 @@ $(document).ready(function(){
         };//end loop
         $('#card-container-Pure').html(displayCardPure);
       }
+
+      let displayCard1Star = "";
+      for(var i = 0; i < data.results.length;i++){
+        let results = data.results[i];
+        let power_min = results.performance_min + results.technique_min + results.technique_min;
+        let power_max = results.performance_max + results.technique_max + results.technique_max;
+        let power_trained = results.performance_trained_max + results.technique_trained_max + results.technique_trained_max;
+
+        if (results.i_rarity == 1){
+          displayCard1Star = `${displayCard1Star} <div class="card" id="Card_${i}" style="width: 30rem;">
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner" id="cardsCarousel">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" />
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" />
+                  </div>
+                  <div id="stats">
+                    <p>Total Untrained Power: ${power_min} to ${power_max}<br>
+                    Performance: ${results.performance_min} to ${results.performance_max}<br>
+                    Technique: ${results.technique_min} to ${results.technique_max}<br>
+                    Visual: ${results.technique_min} to ${results.visual_max}</p>
+
+                    <p>Total Trained Power: ${power_trained}<br>
+                    Performance: ${results.performance_trained_max}<br>
+                    Technique: ${results.technique_trained_max}<br>
+                    Visual: ${results.visual_trained_max}</p>
+                  </div>
+                </div>
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              Rarity: ${results.i_rarity}★<br>
+              Attribute: ${results.i_attribute}<br>
+              Skill: ${results.skill_name} (${results.japanese_skill_name})
+            </h6>
+            <p class="card-text">${results.full_skill}</p>
+
+            <button type="button" class="btn btn-dark" id="addCard_${i}">Click here to add My List</button>
+            </div>
+            </div>`
+
+        };//end loop
+        $('#card-container-1Star').html(displayCard1Star);
+      }
+
+      let displayCard2Star = "";
+      for(var i = 0; i < data.results.length;i++){
+        let results = data.results[i];
+        let power_min = results.performance_min + results.technique_min + results.technique_min;
+        let power_max = results.performance_max + results.technique_max + results.technique_max;
+        let power_trained = results.performance_trained_max + results.technique_trained_max + results.technique_trained_max;
+
+        if (results.i_rarity == 2){
+          displayCard2Star = `${displayCard2Star} <div class="card" id="Card_${i}" style="width: 30rem;">
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner" id="cardsCarousel">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" />
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" />
+                  </div>
+                  <div id="stats">
+                    <p>Total Untrained Power: ${power_min} to ${power_max}<br>
+                    Performance: ${results.performance_min} to ${results.performance_max}<br>
+                    Technique: ${results.technique_min} to ${results.technique_max}<br>
+                    Visual: ${results.technique_min} to ${results.visual_max}</p>
+
+                    <p>Total Trained Power: ${power_trained}<br>
+                    Performance: ${results.performance_trained_max}<br>
+                    Technique: ${results.technique_trained_max}<br>
+                    Visual: ${results.visual_trained_max}</p>
+                  </div>
+                </div>
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              Rarity: ${results.i_rarity}★<br>
+              Attribute: ${results.i_attribute}<br>
+              Skill: ${results.skill_name} (${results.japanese_skill_name})
+            </h6>
+            <p class="card-text">${results.full_skill}</p>
+
+            <button type="button" class="btn btn-dark" id="addCard_${i}">Click here to add My List</button>
+            </div>
+            </div>`
+
+        };//end loop
+        $('#card-container-2Star').html(displayCard2Star);
+      }
+
+      let displayCard3Star = "";
+      for(var i = 0; i < data.results.length;i++){
+        let results = data.results[i];
+        let power_min = results.performance_min + results.technique_min + results.technique_min;
+        let power_max = results.performance_max + results.technique_max + results.technique_max;
+        let power_trained = results.performance_trained_max + results.technique_trained_max + results.technique_trained_max;
+
+        if (results.i_rarity == 3){
+          displayCard3Star = `${displayCard3Star} <div class="card" id="Card_${i}" style="width: 30rem;">
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner" id="cardsCarousel">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" />
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" />
+                  </div>
+                  <div id="stats">
+                    <p>Total Untrained Power: ${power_min} to ${power_max}<br>
+                    Performance: ${results.performance_min} to ${results.performance_max}<br>
+                    Technique: ${results.technique_min} to ${results.technique_max}<br>
+                    Visual: ${results.technique_min} to ${results.visual_max}</p>
+
+                    <p>Total Trained Power: ${power_trained}<br>
+                    Performance: ${results.performance_trained_max}<br>
+                    Technique: ${results.technique_trained_max}<br>
+                    Visual: ${results.visual_trained_max}</p>
+                  </div>
+                </div>
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              Rarity: ${results.i_rarity}★<br>
+              Attribute: ${results.i_attribute}<br>
+              Skill: ${results.skill_name} (${results.japanese_skill_name})
+            </h6>
+            <p class="card-text">${results.full_skill}</p>
+
+            <button type="button" class="btn btn-dark" id="addCard_${i}">Click here to add My List</button>
+            </div>
+            </div>`
+
+        };//end loop
+        $('#card-container-3Star').html(displayCard3Star);
+      }
+
+      let displayCard4Star = "";
+      for(var i = 0; i < data.results.length;i++){
+        let results = data.results[i];
+        let power_min = results.performance_min + results.technique_min + results.technique_min;
+        let power_max = results.performance_max + results.technique_max + results.technique_max;
+        let power_trained = results.performance_trained_max + results.technique_trained_max + results.technique_trained_max;
+
+        if (results.i_rarity == 4){
+          displayCard4Star = `${displayCard4Star} <div class="card" id="Card_${i}" style="width: 30rem;">
+            <div id="carousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner" id="cardsCarousel">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" />
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" />
+                  </div>
+                  <div id="stats">
+                    <p>Total Untrained Power: ${power_min} to ${power_max}<br>
+                    Performance: ${results.performance_min} to ${results.performance_max}<br>
+                    Technique: ${results.technique_min} to ${results.technique_max}<br>
+                    Visual: ${results.technique_min} to ${results.visual_max}</p>
+
+                    <p>Total Trained Power: ${power_trained}<br>
+                    Performance: ${results.performance_trained_max}<br>
+                    Technique: ${results.technique_trained_max}<br>
+                    Visual: ${results.visual_trained_max}</p>
+                  </div>
+                </div>
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
+            <h6 class="card-subtitle mb-2 text-muted">
+              Rarity: ${results.i_rarity}★<br>
+              Attribute: ${results.i_attribute}<br>
+              Skill: ${results.skill_name} (${results.japanese_skill_name})
+            </h6>
+            <p class="card-text">${results.full_skill}</p>
+
+            <button type="button" class="btn btn-dark" id="addCard_${i}">Click here to add My List</button>
+            </div>
+            </div>`
+
+        };//end loop
+        $('#card-container-4Star').html(displayCard4Star);
+      }
+
     });
-    
 });
---
+++ test.js
@@ -0,0 +1,61 @@
+for(var i = 0; i < data.results.length;i++){
+          let results = data.results[i];
+          let power_min = results.performance_min + results.technique_min + results.technique_min
+          let power_max = results.performance_max + results.technique_max + results.technique_max
+          let power_trained = results.performance_trained_max + results.technique_trained_max + results.technique_trained_max
+
+          display = `${display}
+              <div class="card" style="width: 35rem;">
+                <div class="d-flex justify-content-center"
+
+                  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner" id="cardsCarousel">
+                      <div class="carousel-item active">
+                        <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" id="cardimage1" />
+                      </div>
+                      <div class="carousel-item">
+                        <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" id="cardimage2" />
+                      </div>
+                      <div class="d-flex justify-content-center" id="stats">
+                       <h5>Total Untrained Power: ${power_min} to ${power_max}</h5>
+                       <p>Performance: ${results.performance_min} to ${results.performance_max}<br>
+                       Technique: ${results.technique_min} to ${results.technique_max}<br>
+                       Visual: ${results.technique_min} to ${results.visual_max}</p>
+
+                       <h5>Total Trained Power: ${power_trained}</h5>
+                       <p>Performance: ${results.performance_trained_max}<br>
+                       Technique: ${results.technique_trained_max}<br>
+                       Visual: ${results.visual_trained_max}</p>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="card-body">
+                    <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Rarity: ${result.i_rarity}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Attribute: ${result.i_attribute}</h6>
+                    <h6 class="card-subtitle mb-2 text-muted">Skill: ${results.skill_name} (${results.japanese_skill_name})</h6>
+                    <p class="card-text">${results.full_skill}</p>
+                  </div>
+
+                  <button type="button" class="btn btn-dark" id="addCard_${i}">Click here to add My List</button>
+            
+                </div>
+              </div>`
+
+        }//end loop
+        //place our entire loop content into the container
+        $('#card-container').html(display);
+
+        // document.addEventListener(mycard, function(event) {
+
+
+        //   if (localStorage.getItem("mycardList") !== null) {
+        //     mycardList = JSON.parse(localStorage.getItem("mycardList"));
+        //   }
+          
+        //   localStorage.setItem(results, JSON.stringify(results))
+          
+        // })
+      });
+});
