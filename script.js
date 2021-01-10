b709a5dda4b644c371533706ae987b7ec59ec003
joliehehehe <s10205048@connect.np.edu.sg>
2021-01-10 12:48:44 +0800

    Update #9: Editted navbar on all pages and updated allcards page

--- script.js
+++ script.js
@@ -1,87 +1,68 @@
 $(document).ready(function(){
     
-    let url = 'https://bandori.party/api/cards/?page=20'
+  let url = 'https://bandori.party/api/cards/?page=3'
 
-    fetch(url)
-      .then(
-        function(response) {
-        return response.json();
-        }
-      )
-    
-      .then(
-        function(data) {
-        //LOOP results
-        let display = "";
-        let mycardList = []
-        for(var i = 0; i < data.results.length;i++){
-          let results = data.results[i];
-          let power_min = results.performance_min + results.technique_min + results.technique_min
-          let power_max = results.performance_max + results.technique_max + results.technique_max
-          let power_trained = results.performance_trained_max + results.technique_trained_max + results.technique_trained_max
+  fetch(url)
+    .then(
+      function(response) {
+      return response.json();
+      }
+    )
+  
+    .then(
+      function(data) {
+       // const results = data.results[0];
+        //console.log(results)
+        //@[cher]
+      //LOOP results
+      let display = "";
+      for(var i = 0; i < data.results.length;i++){
+        let results = data.results[i];
+        let power_min = results.performance_min + results.technique_min + results.technique_min;
+        let power_max = results.performance_max + results.technique_max + results.technique_max;
+        let power_trained = results.performance_trained_max + results.technique_trained_max + results.technique_trained_max;
 
-          display = `${display} 
-              <div class="card" style="width: 35rem;">
-                <div class="d-flex justify-content-center"
-
-                  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                      <div class="carousel-item active">
-                        <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" id="cardimage1" />
-                      </div>
-                      <div class="carousel-item">
-                        <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" id="cardimage2" />
-                      </div>
-                      <div id="stats">
-                       <h5>Total Untrained Power: ${power_min} to ${power_max}</h5>
-                       <p>Performance: ${results.performance_min} to ${results.performance_max}<br>
-                       Technique: ${results.technique_min} to ${results.technique_max}<br>
-                       Visual: ${results.technique_min} to ${results.visual_max}</p>
-
-                       <h5>Total Trained Power: ${power_trained}</h5>
-                       <p>Performance: ${results.performance_trained_max}<br>
-                       Technique: ${results.technique_trained_max}<br>
-                       Visual: ${results.visual_trained_max}</p>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div class="card-body">
-                    <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">Rarity: ${result.i_rarity}</h6>
-                    <h6 class="card-subtitle mb-2 text-muted">Attribute: ${result.i_attribute}</h6>
-                    <h6 class="card-subtitle mb-2 text-muted">Skill: ${results.skill_name} (${results.japanese_skill_name})</h6>
-                    <p class="card-text">${results.full_skill}</p>
-                  </div>
-
-                  <button type="button" class="btn btn-dark" id="addCard">Click here to add My List</button>
-            
+        display = `${display} <div class="card" style="width: 30rem;">
+          <div id="carousel" class="carousel slide" data-ride="carousel">
+              <div class="carousel-inner" id="cardsCarousel">
+                <div class="carousel-item active">
+                  <img class="d-block w-100" src="${results.art}" alt="Card illustration (normal)" />
                 </div>
-              </div>`
+                <div class="carousel-item">
+                  <img class="d-block w-100" src="${results.art_trained}" alt="Card illustration (trained)" />
+                </div>
+                <div id="stats">
+                  <p>Total Untrained Power: ${power_min} to ${power_max}<br>
+                  Performance: ${results.performance_min} to ${results.performance_max}<br>
+                  Technique: ${results.technique_min} to ${results.technique_max}<br>
+                  Visual: ${results.technique_min} to ${results.visual_max}</p>
 
-              if(document.getElementById('addCard').clicked == true)
-              {
-                `${mycardList}`.append(results)
-              }
+                  <p>Total Trained Power: ${power_trained}<br>
+                  Performance: ${results.performance_trained_max}<br>
+                  Technique: ${results.technique_trained_max}<br>
+                  Visual: ${results.visual_trained_max}</p>
+                </div>
+              </div>
+          </div>
+          <div class="card-body">
+          <h5 class="card-title">Name: ${results.name} (${results.japanese_name})</h5>
+          <h6 class="card-subtitle mb-2 text-muted">
+            Rarity: ${results.i_rarity}★<br>
+            Attribute: ${results.i_attribute}<br>
+            Skill: ${results.skill_name} (${results.japanese_skill_name})
+          </h6>
+          <p class="card-text">${results.full_skill}</p>
 
-        }//end loop
-        //place our entire loop content into the container
-        $('#card-container').html(display);
+          <button type="button" class="btn btn-dark" id="addCard_${i}">Click here to add My List</button>
+          </div>
+          </div>`
+      }//end loop
+      //place our entire loop content into the container
+      $('#card-container').html(display);
 
         
-
-
-
-
-        document.addEventListener(mycard, function(event) {
-
-
-          if (localStorage.getItem("mycardList") !== null) {
-            mycardList = JSON.parse(localStorage.getItem("mycardList"));
-          }
-          
-          localStorage.setItem(results, JSON.stringify(results))
-          
-        })
-      });
+    });
+    $('.carousel').carousel({
+      interval: 2000
+    })
 });
